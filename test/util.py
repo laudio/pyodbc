152961c1e0566788021f092efdcc726b9952237d
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-09-06 23:17:15 +0545

    Prefix environment vars with TEST_

--- test/util.py
+++ test/util.py
@@ -16,10 +16,10 @@ def mssql_exec_query(sql):
     ''' Execute a test SQL query on the database. '''
     connection_str = MSSQL_CONNECTION_STRING.format(
         driver=MSSQL_ODBC_DRIVER,
-        server=os.environ['MSSQL_DB_HOST'],
-        database=os.environ['MSSQL_DB_NAME'],
-        username=os.environ['MSSQL_DB_USER'],
-        password=os.environ['MSSQL_DB_PASSWORD'],
+        server=os.environ['TEST_MSSQL_DB_HOST'],
+        database=os.environ['TEST_MSSQL_DB_NAME'],
+        username=os.environ['TEST_MSSQL_DB_USER'],
+        password=os.environ['TEST_MSSQL_DB_PASSWORD'],
         port=1433
     )
 
