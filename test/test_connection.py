0febb328427c2eaef5565ddde90e3a3532c46189
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-09-06 23:32:56 +0545

    Test for postgres connection

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,11 +1,19 @@
 ''' Test MSSQL Connection works. '''
 
-from util import logger, mssql_exec_query
+from util import logger, mssql_exec_query, pg_exec_query
 
 
 def test_mssql():
     ''' Test connection to MSSQL Server. '''
     logger.debug('Starting to connect')
     result = mssql_exec_query("SELECT 'It works!'")
+
+    assert result == 'It works!'
+
+
+def test_pg():
+    ''' Test connection to PostgreSQL Server. '''
+    logger.debug('Starting to connect')
+    result = pg_exec_query("SELECT 'It works!'")
 
     assert result == 'It works!'
