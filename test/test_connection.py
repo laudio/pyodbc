1d48d02e49906be0a9411dbc8560c9387df3cffb
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-09-07 00:42:32 +0545

    Remove debug logs from tests

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,11 +1,10 @@
 ''' Tests for database connections. '''
 
-from util import logger, mssql_exec_query, pg_exec_query
+from util import mssql_exec_query, pg_exec_query
 
 
 def test_mssql():
     ''' Test connection to MSSQL Server. '''
-    logger.debug('Starting to connect')
     result = mssql_exec_query("SELECT 'It works!'")
 
     assert result == 'It works!'
@@ -13,7 +12,6 @@ def test_mssql():
 
 def test_pg():
     ''' Test connection to PostgreSQL Server. '''
-    logger.debug('Starting to connect')
     result = pg_exec_query("SELECT 'It works!'")
 
     assert result == 'It works!'
