8628b68076b1889f34acc501dc1f75473f1edc87
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-09-09 21:05:52 +0545

    Check condition only for the publish block

--- release.sh
+++ release.sh
@@ -2,36 +2,33 @@
 
 set -e
 
-publish() {
-  # Download semver
-  sudo curl https://raw.githubusercontent.com/fsaintjacques/semver-tool/2.1.0/src/semver -o /usr/local/bin/semver && sudo chmod +x /usr/local/bin/semver
+# Download semver
+sudo curl https://raw.githubusercontent.com/fsaintjacques/semver-tool/2.1.0/src/semver -o /usr/local/bin/semver && sudo chmod +x /usr/local/bin/semver
 
-  last_version=$(git tag --sort=-creatordate | head -n 1)
-  new_version=$(semver bump patch "$last_version")
+last_version=$(git tag --sort=-creatordate | head -n 1)
+new_version=$(semver bump patch "$last_version")
 
-  echo "Last Version: $last_version"
-  echo "New Version: $new_version"
+echo "Last Version: $last_version"
+echo "New Version: $new_version"
 
-  git tag "$new_version"
-  git push origin "$new_version"
+git tag "$new_version"
 
-  # Login to Docker
-  echo "${DOCKER_REGISTRY_PASSWORD}" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin
+# Create image tag
+image_tag="${IMAGE_NAME}:${new_version}"
+docker tag "$IMAGE_NAME" "$image_tag"
 
-  # Create image tag
-  image_tag="${IMAGE_NAME}:${new_version}"
-  docker tag "$IMAGE_NAME" "$image_tag"
-
-  echo "Tagged docker image $image_tag"
-
-  # Publish to registry
-  docker push "$image_tag"
-  docker push "${IMAGE_NAME}:latest"
-}
+echo "Tagged docker image $image_tag"
 
 # If the branch is master, publish it.
 if [ "$BRANCH" = "master" ]; then
-  publish
+  # Login to Docker
+  echo "${DOCKER_REGISTRY_PASSWORD}" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin
+
+  # Push the newly tagged image to registry and GitHub
+  docker push "$image_tag"
+  docker push "${IMAGE_NAME}:latest"
+  git push origin "$new_version"
+
   echo "Published image $image_tag"
 else
   echo "Publish skipped"
