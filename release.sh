e4ebdaef9a24ecb51723d0f7dc5e0f345fbbf283
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-09-09 18:35:06 +0545

    Print informative messages

--- release.sh
+++ release.sh
@@ -8,14 +8,24 @@ sudo curl https://raw.githubusercontent.com/fsaintjacques/semver-tool/2.1.0/src/
 last_version=$(git tag --sort=-creatordate | head -n 1)
 new_version=$(semver bump patch "$last_version")
 
-git tag "$new_version"
+echo "Last Version: $last_version"
+echo "New Version: $new_version"
+
+git tag "$new_version" && git tag --sort=-creatordate | head -n 1
+
 # git push origin "$new_version"
 
 # Login to Docker
 echo ${DOCKER_REGISTRY_PASSWORD} | docker login -u $DOCKER_REGISTRY_USERNAME --password-stdin
 
-# Publish to registry
+# Create image tag
 image_tag="${IMAGE_NAME}:${new_version}"
 docker tag "$IMAGE_NAME" "$image_tag"
+
+echo "Tagged docker image $image_tag"
+
+# Publish to registry
 # docker push "$image_tag"
 # docker push "${IMAGE_NAME}:latest"
+
+echo "Published image $image_tag"
