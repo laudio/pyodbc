7edb6091d45e6770970ad7a5840caada951b6cda
v1shwa <v1shwa@users.noreply.github.com>
2019-10-06 12:41:10 +0530

    Use environmental variables for credentials

--- examples/app-mssql/README.md
+++ examples/app-mssql/README.md
@@ -3,5 +3,14 @@ Example project using mssql with pyodbc docker image.
 
 ### Usage
 
-`docker-compose up`
+- Set credentials for the DB in enviromental variables.
+```                                                                      
+export DB_NAME='tempdb';
+export DB_USERNAME='SA';
+export DB_PASSWORD='someP4ssword';
+```
+- Start the containers
 
+```
+docker-compose up
+```
--- examples/app-mssql/main.py
+++ examples/app-mssql/main.py
@@ -1,27 +1,34 @@
-import pyodbc
 import sys
+import os
 import time
+import pyodbc
 
 # waiting for mssql db server to fully spawn
 time.sleep(3)
 
 print("Creating a mssql connection")
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=mssql_server;DATABASE=tempdb;UID=SA;PWD=bad_P4ssword;')
+connection_str = 'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};'.format(
+        server=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        username=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD']
+    )
+conn = pyodbc.connect(connection_str, timeout=300)
 cur = conn.cursor()
 
 
-print("Create a new table: Fruits")
-cur.execute("CREATE TABLE Fruits (id INT, name NVARCHAR(50), quantity INT);")
+print("Create a new table: fruits")
+cur.execute("CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT);")
 conn.commit()
 
-print("Insert data into `Fruits` table")
-cur.execute("INSERT INTO Fruits VALUES (1, 'banana', 150);")
-cur.execute("INSERT INTO Fruits VALUES (2, 'orange', 64);")
-cur.execute("INSERT INTO Fruits VALUES (2, 'apples', 35);")
+print("Insert data into `fruits` table")
+cur.execute("INSERT INTO fruits VALUES (1, 'banana', 150);")
+cur.execute("INSERT INTO fruits VALUES (2, 'orange', 64);")
+cur.execute("INSERT INTO fruits VALUES (2, 'apples', 35);")
 conn.commit()
 
-print("Select data from `Fruits` table")
-cur.execute("SELECT * FROM Fruits WHERE quantity > 50;")
+print("Select data from `fruits` table")
+cur.execute("SELECT * FROM fruits WHERE quantity > 50;")
 rows = cur.fetchall()
 for row in rows:
     print(row.id, row.name, row.quantity)
