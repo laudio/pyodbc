9678c2dda547fd283a0ce1f635430acc3cc30307
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-10-08 00:36:39 +0545

    Make the example functional

--- examples/app-mssql/main.py
+++ examples/app-mssql/main.py
@@ -1,40 +1,48 @@
+''' Main module. '''
 import sys
 import os
 import time
 import pyodbc
 
-# waiting for mssql db server to fully spawn
-time.sleep(3)
+CONNECTION_STRING = 'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};'
 
-print("Creating a mssql connection")
-connection_str = 'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};'.format(
+
+def main():
+    ''' App entrypoint. '''
+    # Wait for mssql db server to fully spawn.
+    time.sleep(5)
+
+    print("Establishing mssql database connection")
+    connection_str = CONNECTION_STRING.format(
         server=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
         username=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD']
     )
-conn = pyodbc.connect(connection_str, timeout=300)
-cur = conn.cursor()
+    conn = pyodbc.connect(connection_str, timeout=300)
+    cur = conn.cursor()
+
+    print("Create a new table: fruits")
+    cur.execute("CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT);")
+    conn.commit()
+
+    print("Insert data into `fruits` table")
+    cur.execute("INSERT INTO fruits VALUES (1, 'banana', 150);")
+    cur.execute("INSERT INTO fruits VALUES (2, 'orange', 64);")
+    cur.execute("INSERT INTO fruits VALUES (2, 'apples', 35);")
+    conn.commit()
+
+    print("Select data from `fruits` table")
+    cur.execute("SELECT * FROM fruits WHERE quantity > 50;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row.id, row.name, row.quantity)
+
+    print("Closing the connection")
+    cur.close()
+    conn.close()
+
+    sys.exit(0)
 
 
-print("Create a new table: fruits")
-cur.execute("CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT);")
-conn.commit()
-
-print("Insert data into `fruits` table")
-cur.execute("INSERT INTO fruits VALUES (1, 'banana', 150);")
-cur.execute("INSERT INTO fruits VALUES (2, 'orange', 64);")
-cur.execute("INSERT INTO fruits VALUES (2, 'apples', 35);")
-conn.commit()
-
-print("Select data from `fruits` table")
-cur.execute("SELECT * FROM fruits WHERE quantity > 50;")
-rows = cur.fetchall()
-for row in rows:
-    print(row.id, row.name, row.quantity)
-
-print("Closing the connection")
-cur.close()
-conn.close()
-
-sys.exit(0)
+main()
