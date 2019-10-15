6c55aecabb73813abe7167602d08c2a5bd13509a
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-10-15 22:07:49 +0545

    Clean output

--- examples/app-mssql/main.py
+++ examples/app-mssql/main.py
@@ -12,7 +12,7 @@ def main():
     # Wait for mssql database server to fully spawn.
     time.sleep(5)
 
-    print('Establishing mssql database connection')
+    print('Establishing mssql database connection.')
     connection_str = CONNECTION_STRING.format(
         server=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
@@ -22,24 +22,27 @@ def main():
     conn = pyodbc.connect(connection_str, timeout=300)
     cur = conn.cursor()
 
-    print('Create a new table for fruits')
+    print('Create a new table for fruits.')
     cur.execute('CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT)')
     conn.commit()
 
     print('Populate fruits data.')
-    cur.execute('INSERT INTO fruits VALUES (1, ?, ?)', ('banana', 150))
-    cur.execute('INSERT INTO fruits VALUES (2, ?, ?)', ('orange', 64))
-    cur.execute('INSERT INTO fruits VALUES (2, ?, ?)', ('apples', 35))
+    cur.execute('INSERT INTO fruits VALUES (1, ?, ?)', ('Banana', 150))
+    cur.execute('INSERT INTO fruits VALUES (2, ?, ?)', ('Orange', 64))
+    cur.execute('INSERT INTO fruits VALUES (3, ?, ?)', ('Apple', 35))
     conn.commit()
 
-    print('Retrieve the data')
+    print('List of data.')
     cur.execute('SELECT * FROM fruits')
     rows = cur.fetchall()
 
+    template = '{:<5} {:<15} {:<10}'
+    print(template.format('ID', 'NAME', 'QUANTITY'))
+    print('-' * 32)
     for row in rows:
-        print(row.id, row.name, row.quantity)
+        print(template.format(row.id, row.name, row.quantity))
 
-    print('Closing the connection')
+    print('Closing the connection.')
     cur.close()
     conn.close()
 
