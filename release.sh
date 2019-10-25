ccf44d720817714cb15b758a6018bbcbbb85256c
Antoni Florit Homar <toniflorithomar@gmail.com>
2019-10-25 20:43:46 +0200

    Create crontol verison, only generate version when change some files

--- release.sh
+++ release.sh
@@ -8,6 +8,8 @@ timestamp=$(date -u +%Y%m%d%H%M%S)
 ref=$(echo "$BRANCH" | sed -e "s/[^a-zA-Z0-9]//g")
 new_version=$(if [ "$ref" = "master" ]; then echo "${new_tag}"; else echo "${new_tag}-${ref}.$timestamp"; fi)
 
+sensible_files=(Dockerfile .dockerignore requirements.txt)
+
 echo "Bump version: ${last_tag} -> ${new_version}"
 git tag "${new_version}"
 
@@ -17,17 +19,23 @@ docker tag "${IMAGE_NAME}" "${image_tag}"
 
 echo "Tagged docker image $image_tag"
 
+
 # If the branch is master, publish it.
 if [ "$ref" = "master" ]; then
-  # Login to Docker
-  echo "${DOCKER_REGISTRY_PASSWORD}" | docker login -u "${DOCKER_REGISTRY_USERNAME}" --password-stdin
-
-  # Push the newly tagged image to registry and GitHub
-  docker push "${image_tag}"
-  docker push "${IMAGE_NAME}:latest"
-  hub release create "${new_version}" -m "${new_version}" || true
-
-  echo "Published $image_tag"
+  for Item in ${sensible_files[*]} ;
+  do
+    compare_items=$(git diff --name-only HEAD~1..HEAD | grep $Item | grep -v example)
+    if [ "$Item" = "$compare_items" ] ; then
+      # Login to Docker
+      echo "${DOCKER_REGISTRY_PASSWORD}" | docker login -u "${DOCKER_REGISTRY_USERNAME}" --password-stdin
+      # Push the newly tagged image to registry and GitHub
+      docker push "${image_tag}"
+      docker push "${IMAGE_NAME}:latest"
+      hub release create "${new_version}" -m "${new_version}" || true
+      echo "Publish skipped"
+      continue
+    fi
+  done
 else
   echo "Publish skipped"
 fi
