1f37395d6c1dd6c4326fccf095aa48980d781bd9
razzkumar <razzkumar.dev@gmail.com>
2019-10-26 16:00:51 +0545

    Fixes after review

--- examples/data-transfer-mssql/README.md
+++ examples/data-transfer-mssql/README.md
@@ -1,7 +1,6 @@
-# MSSQL Example
+# Data Transfer for MSSQL
 
-Example project using mssql with pyodbc docker image. which take a data from
-one db and insert to another table
+Example project to demonstrate a data pipeline container built using laudio/pyodbc as a base image - that transfers data from one database to another database
 
 ### Running
 
@@ -31,15 +30,15 @@ The output of the application should look like this.
 app_1                | Establishing mssql database connection to source db
 app_1                | Create a new table for fruits.
 app_1                | Populate fruits data.
-app_1                | ---selecting data from source db----
-app_1                | ---closing source db----
+app_1                | importing data from source db
+app_1                | closing source db
 app_1                | Establishing mssql database connection to destination db
 app_1                | 
 app_1                | 
 app_1                | 
-app_1                | Create a new table for fruits. on destination db
-app_1                | Inserting data to destination db
-app_1                | ----------------List of data from destination.-----------------------
+app_1                | Create table on the destination database.
+app_1                | Transferring data into destination database.
+app_1                | Data in the destination database.
 app_1                | ID    NAME            QUANTITY  
 app_1                | --------------------------------
 app_1                | 1     Banana          150       
@@ -49,5 +48,5 @@ app_1                |
 app_1                | 
 app_1                | 3 rows transferred
 app_1                | 
-app_1                | --- Closing the destination db ---.
+app_1                | Closing the destination db.
 ```
--- examples/data-transfer-mssql/main.py
+++ examples/data-transfer-mssql/main.py
@@ -6,7 +6,6 @@ import pyodbc
 
 CONNECTION_STRING = 'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};'
 
-
 def main():
     ''' App entrypoint. '''
     # Wait for mssql database server to fully spawn.
@@ -14,14 +13,9 @@ def main():
 
     # Source
     print('Establishing mssql database connection to source db')
-    source_connection_str = CONNECTION_STRING.format(
-        server=os.environ['SOURCE_DB_HOST'],
-        database=os.environ['DB_NAME'],
-        username=os.environ['DB_USERNAME'],
-        password=os.environ['DB_PASSWORD']
-    )
     
-    source_conn = pyodbc.connect(source_connection_str, timeout=300)
+    source_conn = get_connection(os.environ['SOURCE_DB_HOST'], os.environ['SOURCE_DB_NAME'],
+                                 os.environ['SOURCE_DB_USERNAME'], os.environ['SOURCE_DB_PASSWORD'])
     cur1 = source_conn.cursor()
 
     print('Create a new table for fruits.')
@@ -35,34 +29,28 @@ def main():
     cur1.execute('INSERT INTO fruits VALUES (3, ?, ?)', ('Apple', 35))
     source_conn.commit()
 
-    print('---selecting data from source db----')
+    print('importing data from source db')
     cur1.execute('SELECT * FROM fruits')
     source_rows = cur1.fetchall()
     
-    print('---closing source db----')
+    print('closing source db')
     cur1.close()
     source_conn.close()
 
 
     # Destination
     print('Establishing mssql database connection to destination db')
-  
-    destination_connection_str = CONNECTION_STRING.format(
-        server=os.environ['DESTINATION_DB_HOST'],
-        database=os.environ['DB_NAME'],
-        username=os.environ['DB_USERNAME'],
-        password=os.environ['DB_PASSWORD']
-    )
 
-    destination_conn = pyodbc.connect(destination_connection_str, timeout=300)
+    destination_conn = get_connection(os.environ['DESTINATION_DB_HOST'], os.environ['DESTINATION_DB_NAME'],
+                                      os.environ['DESTINATION_DB_USERNAME'], os.environ['DESTINATION_DB_PASSWORD'])
     cur2 = destination_conn.cursor()
 
-    print('\n\n\nCreate a new table for fruits. on destination db')
+    print('\n\n\nCreate table on the destination database.')
 
     cur2.execute('CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT)')
     destination_conn.commit()
 
-    print('Inserting data to destination db')
+    print('Transferring data into destination database.')
     for row_data in source_rows:
         cur2.execute('INSERT INTO fruits VALUES (?, ?, ?)', (row_data.id,row_data.name,row_data.quantity))
         destination_conn.commit()
@@ -70,20 +58,31 @@ def main():
     cur2.execute('SELECT * FROM fruits')
     destination_rows = cur2.fetchall()
 
-    print('----------------List of data from destination.-----------------------')
+    print('Data in the destination database.')
     template = '{:<5} {:<15} {:<10}'
     print(template.format('ID', 'NAME', 'QUANTITY'))
     print('-' * 32)
     for row in destination_rows:
         print(template.format(row.id, row.name, row.quantity))
 
-    print("\n")
+    print('\n')
     print(f'{len(destination_rows)} rows transferred\n')
-    print('--- Closing the destination db ---.')
+    print('Closing the destination db.')
     cur2.close()
     destination_conn.close()
 
     sys.exit(0)
 
+
+def get_connection(db_host,db_name,db_username,db_password):
+
+    connection_str = CONNECTION_STRING.format(
+        server=db_host,
+        database=db_name,
+        username=db_username,
+        password=db_password
+    )
+
+    return pyodbc.connect(connection_str, timeout=300)
 
 main()
