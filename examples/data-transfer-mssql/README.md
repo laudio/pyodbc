ff8582f10b2c54f11f71ef1ff2432f23850adc38
razzkumar <razzkumar.dev@gmail.com>
2019-10-30 10:49:07 +0545

    Fixes to make identical to PR #19

--- examples/data-transfer-mssql/README.md
+++ examples/data-transfer-mssql/README.md
@@ -27,26 +27,4 @@ $ docker-compose up --build
 The output of the application should look like this.
 
 ```
-app_1                | Establishing mssql database connection to source db
-app_1                | Create a new table for fruits.
-app_1                | Populate fruits data.
-app_1                | importing data from source db
-app_1                | closing source db
-app_1                | Establishing mssql database connection to destination db
-app_1                | 
-app_1                | 
-app_1                | 
-app_1                | Create table on the destination database.
-app_1                | Transferring data into destination database.
-app_1                | Data in the destination database.
-app_1                | ID    NAME            QUANTITY  
-app_1                | --------------------------------
-app_1                | 1     Banana          150       
-app_1                | 2     Orange          64        
-app_1                | 3     Apple           35        
-app_1                | 
-app_1                | 
-app_1                | 3 rows transferred
-app_1                | 
-app_1                | Closing the destination db.
 ```
--- examples/data-transfer-mssql/main.py
+++ examples/data-transfer-mssql/main.py
@@ -11,66 +11,46 @@ def main():
     # Wait for mssql database server to fully spawn.
     time.sleep(5)
 
-    # Source
-    print('Establishing mssql database connection to source db')
+    source_db_conn, dest_db_conn = connect_to_databases()
+
+    with source_db_conn, dest_db_conn:
+
+        source_db_cur = source_db_conn.cursor()
+        dest_db_cur = dest_db_conn.cursor()
+
+        with source_db_cur, dest_db_cur:
+
+            print(f'Create fruits table and populate data in source database.')
+            source_db_cur.execute(extract_sql('sql/source_db_setup.sql'))
+            source_db_conn.commit()
+
+            print(f'Create fruits table in destination database.')
+            dest_db_cur.execute(extract_sql('sql/dest_db_setup.sql'))
+            dest_db_conn.commit()
+
+            transfer_data(source_db_cur, dest_db_cur, dest_db_conn)
+
+            print(f'Display fruits data of destination database.')
+            display_fruits(dest_db_cur)
+
+
+def connect_to_databases():
+    ''' Extracts databases credentials from the environment and returns their connections.'''
+    source_db_conn = get_connection(
+            os.environ['SOURCE_DB_HOST'],
+            os.environ['SOURCE_DB_NAME'],
+            os.environ['SOURCE_DB_USERNAME'],
+            os.environ['SOURCE_DB_PASSWORD']
+        )
+
+    dest_db_conn = get_connection(
+            os.environ['DESTINATION_DB_HOST'],
+            os.environ['DESTINATION_DB_NAME'],
+            os.environ['DESTINATION_DB_USERNAME'],
+            os.environ['DESTINATION_DB_PASSWORD']
+        )
     
-    source_conn = get_connection(os.environ['SOURCE_DB_HOST'], os.environ['SOURCE_DB_NAME'],
-                                 os.environ['SOURCE_DB_USERNAME'], os.environ['SOURCE_DB_PASSWORD'])
-    cur1 = source_conn.cursor()
-
-    print('Create a new table for fruits.')
-    cur1.execute('CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT)')
-    source_conn.commit()
-
-
-    print('Populate fruits data.')
-    cur1.execute('INSERT INTO fruits VALUES (1, ?, ?)', ('Banana', 150))
-    cur1.execute('INSERT INTO fruits VALUES (2, ?, ?)', ('Orange', 64))
-    cur1.execute('INSERT INTO fruits VALUES (3, ?, ?)', ('Apple', 35))
-    source_conn.commit()
-
-    print('importing data from source db')
-    cur1.execute('SELECT * FROM fruits')
-    source_rows = cur1.fetchall()
-    
-    print('closing source db')
-    cur1.close()
-    source_conn.close()
-
-
-    # Destination
-    print('Establishing mssql database connection to destination db')
-
-    destination_conn = get_connection(os.environ['DESTINATION_DB_HOST'], os.environ['DESTINATION_DB_NAME'],
-                                      os.environ['DESTINATION_DB_USERNAME'], os.environ['DESTINATION_DB_PASSWORD'])
-    cur2 = destination_conn.cursor()
-
-    print('\n\n\nCreate table on the destination database.')
-
-    cur2.execute('CREATE TABLE fruits (id INT, name NVARCHAR(50), quantity INT)')
-    destination_conn.commit()
-
-    print('Transferring data into destination database.')
-    for row_data in source_rows:
-        cur2.execute('INSERT INTO fruits VALUES (?, ?, ?)', (row_data.id,row_data.name,row_data.quantity))
-        destination_conn.commit()
-
-    cur2.execute('SELECT * FROM fruits')
-    destination_rows = cur2.fetchall()
-
-    print('Data in the destination database.')
-    template = '{:<5} {:<15} {:<10}'
-    print(template.format('ID', 'NAME', 'QUANTITY'))
-    print('-' * 32)
-    for row in destination_rows:
-        print(template.format(row.id, row.name, row.quantity))
-
-    print('\n')
-    print(f'{len(destination_rows)} rows transferred\n')
-    print('Closing the destination db.')
-    cur2.close()
-    destination_conn.close()
-
+    return source_db_conn, dest_db_conn
 
 def get_connection(db_host,db_name,db_username,db_password):
 
@@ -83,9 +63,42 @@ def get_connection(db_host,db_name,db_username,db_password):
     
     return pyodbc.connect(connection_str, timeout=300)
 
-main()
+
+def extract_sql(file: str):
+    ''' Reads an SQL file and returns it's contents.'''
+    with open(file, 'rt') as file:
+        contents = file.read()
+    return contents
+
+
+def transfer_data(source_db_cursor, dest_db_cursor, dest_db_conn):
+    ''' Extracts fruits data from source database and stores them in destination database.'''
+    print(f'Extracting fruits data from source database.')
+    source_db_cursor.execute('SELECT * FROM fruits')
+    rows = source_db_cursor.fetchall()
+
+    print(f'Transferring fruits data to destination database.')
+    for row in rows:
+        dest_db_cursor.execute('INSERT INTO fruits VALUES (?, ?, ?)', (row.id, row.name, row.quantity))
+    
+    print(f'{len(rows)} rows transferred\n')
+    
+    dest_db_conn.commit()
+
+
+def display_fruits(db_cursor):
+    ''' Displays fruits data. '''
+    db_cursor.execute('SELECT * FROM fruits')
+    transferred_data = db_cursor.fetchall()
+    template = '{:<5} {:<15} {:<10}'
+
+    print(template.format('ID', 'NAME', 'QUANTITY'))
+    print('-' * 32)
+
+    for row in transferred_data:
+        print(template.format(row.id, row.name, row.quantity))
+
 
 if __name__ == '__main__':
     main()
-
     sys.exit(0)
