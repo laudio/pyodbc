6c597a9975dd64e960afc1a846a3c4a669918ee9
yankeexe <yankee.exe@gmail.com>
2019-11-24 21:35:16 +0545

    Generate 10000 data for app-pg data transfer

--- examples/data-transfer-pg/main.py
+++ examples/data-transfer-pg/main.py
@@ -3,8 +3,13 @@ import sys
 import os
 import time
 import pyodbc
+from faker import Faker
+from typing import List, Tuple
+
 
 CONNECTION_STRING = 'DRIVER={{PostgreSQL Unicode}};SERVER={server};DATABASE={database};UID={username};PWD={password};'
+RECORD_COUNT = 10000
+SQL_INSERT_DATA = 'INSERT INTO users (id, name, city) VALUES ({id}, {name}, {city});'
 
 
 def main():
@@ -13,6 +18,8 @@ def main():
     time.sleep(5)
 
     source_db_conn, dest_db_conn = connect_to_databases()
+    data = get_data(RECORD_COUNT)
+    load_data  = insert_source_db(data)
 
     with source_db_conn, dest_db_conn:
 
@@ -21,18 +28,18 @@ def main():
 
         with source_db_cur, dest_db_cur:
 
-            print(f'Create fruits table and populate data in source database.')
+            print(f'Create users table and populate data in source database.')
             source_db_cur.execute(extract_sql('sql/source_db_setup.sql'))
             source_db_conn.commit()
 
-            print(f'Create fruits table in destination database.')
+            print(f'Create users table in destination database.')
             dest_db_cur.execute(extract_sql('sql/dest_db_setup.sql'))
             dest_db_conn.commit()
 
             transfer_data(source_db_cur, dest_db_cur, dest_db_conn)
 
-            print(f'Display fruits data in destination database.')
-            display_fruits(dest_db_cur)
+            print(f'Display users data in destination database.')
+            display_users(dest_db_cur)
 
 
 def get_connection(host: str, db_name: str, db_user: str, db_password: str): 
@@ -62,41 +69,61 @@ def connect_to_databases():
             os.environ['DB2_USERNAME'], 
             os.environ['DB2_PASSWORD']
         )
+
     return source_db_conn, dest_db_conn
+
+
+def get_data(count: int) -> List[Tuple]:
+    ''' Generate user data. '''
+    fake = Faker()
+    row = lambda n: (n + 1, repr(fake.name()), repr(fake.city()))
+
+    return [row(i) for i in range(count)]
+
+
+def insert_source_db(data):
+    for i in data: 
+        with open('sql/source_db_setup.sql', 'a') as file:
+            file.write(SQL_INSERT_DATA.format(
+            id = i[0], 
+            name = i[1], 
+            city = i[2]
+        )+ '\n')
 
 
 def extract_sql(file: str):
     ''' Reads an SQL file and returns it's contents.'''
     with open (file, 'rt') as file:
         contents = file.read()  
+
     return contents
 
 
 def transfer_data(source_db_cursor, dest_db_cursor, dest_db_conn):
-    ''' Extracts fruits data from source database and stores them in destination database.'''
-    print(f'Extracting fruits data from source database.')
-    source_db_cursor.execute('SELECT * FROM fruits')
+    ''' Extracts users data from source database and stores them in destination database.'''
+    print(f'Extracting users data from source database.')
+    source_db_cursor.execute('SELECT * FROM users')
     rows = source_db_cursor.fetchall()
 
-    print(f'Transferring fruits data to destination database.')
+    print(f'Transferring users data to destination database.')
     for row in rows:
-        dest_db_cursor.execute('INSERT INTO fruits VALUES (?, ?, ?)', (row.id, row.name, row.quantity))
+        dest_db_cursor.execute('INSERT INTO users VALUES (?, ?, ?)', (row.id, row.name, row.city))
     dest_db_conn.commit()
     
-    print(f"Transferred {len(rows)} rows of fruits data from source database to destination database.")
+    print(f"Transferred {len(rows)} rows of users data from source database to destination database.")
 
 
-def display_fruits(db_cursor):
-    ''' Displays fruits data. '''
-    db_cursor.execute('SELECT * FROM fruits')
+def display_users(db_cursor):
+    ''' Displays users data. '''
+    db_cursor.execute('SELECT * FROM users')
     transfered_data = db_cursor.fetchall()
     template = '{:<5} {:<15} {:<10}'
 
-    print(template.format('ID', 'NAME', 'QUANTITY'))
+    print(template.format('ID', 'NAME', 'CITY'))
     print('-' * 32)
 
     for row in transfered_data:
-        print(template.format(row.id, row.name, row.quantity))
+        print(template.format(row.id, row.name, row.city))
 
 
 if __name__ == '__main__':
