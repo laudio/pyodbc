dcf2bde590ffdaa7b49af9067681558abbf6b44c
yankeexe <yankee.exe@gmail.com>
2019-11-25 22:36:03 +0545

    Change name of table and column in source db

--- examples/data-transfer-mssql/sql/source_db_setup.sql
+++ examples/data-transfer-mssql/sql/source_db_setup.sql
@@ -1,7 +1,7 @@
 -- Create table for fruits data (source database).
-CREATE TABLE users (id INT, name VARCHAR(50), quantity VARCHAR(50));
+CREATE TABLE users (id INT, name VARCHAR(50), city VARCHAR(50));
 
 -- Insert data into fruits table
-INSERT INTO users VALUES (1, 'Banana', 200);
-INSERT INTO users VALUES (2, 'Orange', 20);
-INSERT INTO users VALUES (3, 'Apple', 350);
+-- INSERT INTO users VALUES (1, 'Banana', 200);
+-- INSERT INTO users VALUES (2, 'Orange', 20);
+-- INSERT INTO users VALUES (3, 'Apple', 350);
