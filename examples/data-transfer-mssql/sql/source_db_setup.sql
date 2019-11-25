3651114c58bdb6afa6f684e560b6e23b22f9790d
yankeexe <yankee.exe@gmail.com>
2019-11-26 00:54:11 +0545

    Remove unused sql queries from source db

--- examples/data-transfer-mssql/sql/source_db_setup.sql
+++ examples/data-transfer-mssql/sql/source_db_setup.sql
@@ -1,7 +1,2 @@
 -- Create table for fruits data (source database).
 CREATE TABLE users (id INT, name VARCHAR(50), city VARCHAR(50));
-
--- Insert data into fruits table
--- INSERT INTO users VALUES (1, 'Banana', 200);
--- INSERT INTO users VALUES (2, 'Orange', 20);
--- INSERT INTO users VALUES (3, 'Apple', 350);
