ce94a1ed769c8fd41c1b53d7c58c931a21e9030c
Saugat Acharya <mesaugat@gmail.com>
2024-02-08 13:42:57 +0545

    Update mssql test to only support ODBC Driver 18

--- images/pyodbc/test/test_mssql.py
+++ images/pyodbc/test/test_mssql.py
@@ -1,11 +1,9 @@
 ''' Tests for MSSQL. '''
-from util import exec_query, MSSQL_17, MSSQL_18
+from util import exec_query, MSSQL
 
 
 def test_connection():
-    ''' Test connection to MSSQL Server using Microsoft ODBC Driver 17 and 18. '''
-    result1 = exec_query(MSSQL_17, "SELECT 'It works!'")
-    result2 = exec_query(MSSQL_18, "SELECT 'It works!'")
+    ''' Test connection to MSSQL Server using Microsoft ODBC Driver. '''
+    result = exec_query(MSSQL, "SELECT 'It works!'")
 
-    assert result1 == 'It works!'
-    assert result2 == 'It works!'
+    assert result == 'It works!'
